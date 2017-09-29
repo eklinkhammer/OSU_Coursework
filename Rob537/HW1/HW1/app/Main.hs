1716738cfd4458b1130ab47369295b82d1540d5b
Eric Klinkhammer <eklinkhammer@gmail.com>
2017-09-28 21:33:33 -0700

    NN

--- Rob537/HW1/HW1/app/Main.hs
+++ Rob537/HW1/HW1/app/Main.hs
@@ -1,6 +1,54 @@
 module Main where
 
 import Lib
+import Numeric.LinearAlgebra
+import Data.Traversable
 
+buildTestNetwork :: NN
+buildTestNetwork = [hidden, output]
+  where
+    hidden = (hw, hb, (sigmoid, sigmoid'))
+    output = (ow, ob, (sigmoid, sigmoid'))
+    hw = (4><3) [0.42,0.88,0.55,
+                 0.1,0.73,0.68,
+                 0.60,0.18,0.47,
+                 0.92,0.11,0.52]
+    hb = (1><3) [0.46,0.72,0.08]
+    ow = (3><1) [0.3,0.25,0.23]
+    ob = (1><1) [0.69]
+
+i1 = vector [1,0,1,0]
+i2 = vector [1,0,1,1]
+i3 = vector [0,1,0,1]
+o1 = vector [1]
+o2 = vector [1]
+o3 = vector [0]
+
+
+oo = (vector [0,0], vector [0])
+ox = (vector [0,1], vector [1])
+xo = (vector [1,0], vector [1])
+xx = (vector [1,1], vector [0])
 main :: IO ()
-main = someFunc
+main = do
+  net <- buildNetwork (sigmoid, sigmoid') [2,2,1]
+  let x = buildTestNetwork
+  printNet x
+  putStrLn $ show (forward x i1)
+  let i = vector [1,1,1,1]
+      (o, info) = forwardWithDeriv i x
+      layer = (head (tail x))
+      outInfo = (head (tail info))
+      (e, l) = backpropLayer 1 (vector [1, 0]) outInfo layer
+      nn = backprop 1 i (vector [1,0]) x
+  -- putStrLn $ show o
+  -- putStrLn $ show info
+  -- putStrLn $ show e
+  -- printNet [l]
+  -- putStrLn $ "Error: " ++ show e
+  -- printNet nn
+  let input = [oo,ox,xo,xx]
+      trained = repTrain 10000 0.01 input net
+  putStrLn $ show $ map (forward net . fst) input
+  printNet trained
+  putStrLn $ show $  map (forward trained . fst) input
--- Rob537/HW1/HW1/src/Lib.hs
+++ Rob537/HW1/HW1/src/Lib.hs
@@ -1,13 +1,98 @@
 module Lib
-    ( someFunc
-    ) where
+    -- ( buildNetwork
+    -- , forward
+    -- , forwardWithDeriv
+    -- , backprop
+    -- , backpropLayer
+    -- , sigmoid
+    -- , sigmoid'
+    -- , printNet
+    -- , repTrain
+    -- , train
+    -- )
+    where
 
 import Numeric.LinearAlgebra
+import Data.Traversable
 
-u = vector [1..5]
+type ActivationF = Double -> Double
+type ActivationD = Double -> Double
+type Activation = (ActivationF, ActivationD)
+type Bias = Matrix R
+type Weights = Matrix R
+type Layer = (Weights, Bias, Activation)
+type NN = [Layer]
 
-v = vector [10,-3,0,4,5]
+type Input = Vector R
+type Output = Vector R
+type Deriv = Vector R
+type Error = Vector R
 
+repTrain :: Int -> LearningRate -> [(Input, Output)] -> NN -> NN
+repTrain 0 _ _ nn = nn
+repTrain n lr ios nn = repTrain (n-1) lr ios (train lr ios nn)
 
-someFunc :: IO ()
-someFunc = putStrLn $ show (u + v)
+train :: LearningRate -> [(Input, Output)] -> NN -> NN
+train lr ios net = foldl (\nn (i,o) -> backprop lr i o nn) net ios
+    
+buildNetwork :: Activation -> [Int] -> IO NN
+buildNetwork act (a:b:xs) = do
+  weights <- randn b a
+  bias    <- randn b 1
+  rest    <- buildNetwork act (b:xs)
+  return $ (weights, bias, act):rest
+buildNetwork _ _ = return []
+
+forward :: NN -> Input -> Output
+forward net x = fst $ forwardWithDeriv x net
+
+layerForward :: Input -> Layer -> (Output, (Input, Deriv))
+layerForward x (w, b, act) = (output, (x, deriv))
+  where
+    output = (activate act . (+) b . (<>) w . asColumn) x
+    deriv = cmap (snd act) output
+
+forwardWithDeriv :: Input -> NN -> (Output, [(Input, Deriv)])
+forwardWithDeriv = mapAccumL layerForward
+
+activate :: Activation -> Matrix R -> Vector R
+activate act = flatten . cmap (fst act)
+
+backprop :: LearningRate -> Input -> Output -> NN -> NN
+backprop lr input y net = backpropLayers lr error inpDs net
+   where
+     (output, inpDs) = forwardWithDeriv input net
+     error = y - output
+
+backpropLayers :: LearningRate -> Error -> [(Input, Deriv)] -> NN -> NN
+backpropLayers lr error inpDs weights = snd $ mapAccumR (backpropLayerTuple lr) error (zip inpDs weights)
+
+backpropLayerTuple :: LearningRate -> Error -> ((Input, Deriv), Layer) -> (Error, Layer)
+backpropLayerTuple lr error (inpD, layer_weights) = backpropLayer lr error inpD layer_weights
+
+type LearningRate = Double
+
+backpropLayer :: LearningRate -> Error -> (Input, Deriv) -> Layer -> (Error, Layer)
+backpropLayer lr error (layer_inp, layer_deriv) layer = (prev_error, new_layer)
+  where
+    (layer_weights, layer_bias, act) = layer
+    new_layer = (updated_weights, new_bias, act)
+    
+    d_layer = layer_deriv * error
+    updated_weights = (+) layer_weights $ cmap (* lr) ((asColumn d_layer) <> (asRow layer_inp))
+    prev_error = flatten $ (<>) (tr layer_weights) (asColumn d_layer)
+    new_bias = (+) layer_bias . asColumn . cmap (* lr) $ d_layer
+    
+
+sigmoid :: Double -> Double
+sigmoid x = 1.0 / (1 + exp (-x))
+
+sigmoid' :: Double -> Double
+sigmoid' x = x * (1 - x)
+
+printNet :: NN -> IO ()
+printNet layers = do
+  mapM_ (\(w,b,_) -> do
+            disp 2 w
+            disp 2 b) layers
+                 
