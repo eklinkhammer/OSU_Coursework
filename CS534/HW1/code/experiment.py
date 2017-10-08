d895658c01bdbb7e666021c9faa62f698c95e556
Eric Klinkhammer <eklinkhammer@gmail.com>
2017-10-07 21:07:34 -0700

    HW1 Perceptron, Mira. Average mira not working.

---
+++ CS534/HW1/code/experiment.py
@@ -0,0 +1,45 @@
+from algs.perceptron import Perceptron
+
+import numpy as np
+
+xs = np.array([[1,-1,-1], [1,1,1], [-1,1,-1], [-1,-1,1]])
+ys = np.array([-1, 1, -1, 1])
+
+def main():
+    perceptron = Perceptron(feature_size=3)
+
+    print ("Before training:")
+    print(perceptron.test(xs, ys))
+    perceptron.train(xs,ys)
+
+    print("After (batch) training:")
+    print(perceptron.test(xs, ys))
+
+    perceptron.reset()
+
+    for j in range(1):
+        for i in range(4):
+            perceptron.train_online(xs[i], ys[i])
+
+    print ("After single training: ")
+    print(perceptron.test(xs,ys))
+
+    print ("Average")
+    perceptron.reset()
+    perceptron.average_train(xs, ys)
+    print(perceptron.test(xs,ys))
+    print ("Naive average")
+    perceptron.reset()
+    perceptron.naive_average_train(xs, ys)
+    print(perceptron.test(xs,ys))
+
+    print ("MIRA")
+    mira = Perceptron(feature_size=3, mira_aggro=0.0)
+    for j in range(10):
+        for i in range(4):
+            mira.train_mira(xs[i], ys[i])
+
+    print(mira.test(xs,ys))
+    
+if __name__ == "__main__":
+    main()
